671cfd0e121b1e0384483d678587813318fe04b0
agman <agman100000000@gmal.com>
2026-08-31 21:47:55 +0300

    Fix Metro Haste module resolution for Android APK build

--- metro.config.js
+++ metro.config.js
@@ -2,13 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.blockList = [
-  /electron-main\.js$/,
-  /server\.js$/,
-  /fastScraper\.js$/,
-  /playwrightScraper\.js$/,
-  /scratch_.*\.js$/,
-  /scratch_.*\.py$/,
-];
-
 module.exports = config;
